cc3c6f658960a44d5ca937846edcd329b0d3fa22
兔爷复仇联盟 <10409074+webps@user.noreply.gitee.com>
2026-06-13 11:57:40 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { supabase } from '@/lib/supabase'
 
 const routes = [
@@ -70,7 +70,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
